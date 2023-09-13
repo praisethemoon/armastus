cd79e3e0d6215113542d94472c8b35b0cbb339f7
praisethemoon <doit@praisethemoon.org>
2023-09-13 14:06:49 +0200

    test++

--- src/PlaceholderComponent.tsx
+++ src/PlaceholderComponent.tsx
@@ -8,7 +8,7 @@ export class PlaceholderComponent extends BaseComponent<{message: string}> {
     render(){
         return (
             <Div key="placeholderRoot" style={{backgroundColor: "#00FF00", width: "100%", height: "100%"}}>
-                <TextBox style={{borderWidth: 10, borderColor: "#000000", width: 200, height: 200}} key="placeholderText" halign="center" valign="center" fontAssetName="defaultFont_30" fontSize={30} textAlign="center">{this.props.message}</TextBox> 
+                <TextBox style={{borderWidth: 10, borderColor: "#000000", width: 200, height: 200}} key="placeholderText" halign="center" valign="center" fontAssetName="defaultFont_30">{this.props.message}</TextBox> 
                 {...this.children}
             </Div>)
     }
--- src/main.tsx
+++ src/main.tsx
@@ -30,9 +30,12 @@ let root: BaseComponent =
         borderRadius: "20px",
     }}>
         <PlaceholderComponent message="hiiii">
-            <Div style={{ width: "200px", height: "200px", backgroundColor: "#35A29F", borderWidth: 2, borderColor: "#000000" , backgroundImageId: "kitten", backgroundImageSize: "cover" }} />
-            <Div style={{ width: "200px", height: "200px", backgroundColor: "#35A29F", borderWidth: 2, borderColor: "#000000" , backgroundImageId: "kitten", backgroundImageSize: "cover" }} />
-            <Div style={{ width: "200px", height: "200px", backgroundColor: "#35A29F", borderWidth: 2, borderColor: "#000000" , backgroundImageId: "kitten", backgroundImageSize: "cover" }} />
+            <Div key={"child2"} style={{ width: "200px", height: "200px", backgroundColor: "#35A29F", borderWidth: 2, borderColor: "#000000" , backgroundImageId: "kitten", backgroundImageSize: "cover" }} />
+            <Div key={"child3"} style={{ width: "200px", height: "200px", backgroundColor: "#35A29F", borderWidth: 2, borderColor: "#000000" , backgroundImageId: "kitten", backgroundImageSize: "cover" }} />
+            <Div key={"child4"} style={{ width: "200px", height: "200px", backgroundColor: "#35A29F", borderWidth: 2, borderColor: "#000000" , backgroundImageId: "kitten", backgroundImageSize: "cover" }} />
+            <Div key={"child5"} style={{ width: "200px", height: "200px", backgroundColor: "#35A29F", borderWidth: 2, borderColor: "#000000" , backgroundImageId: "kitten", backgroundImageSize: "cover" }} />
+            <Div key={"child6"} style={{ width: "200px", height: "200px", backgroundColor: "#35A29F", borderWidth: 2, borderColor: "#000000" , backgroundImageId: "kitten", backgroundImageSize: "cover" }} />
+
         </PlaceholderComponent>
     </Div>
 
