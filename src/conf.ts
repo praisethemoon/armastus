2e8a2281f61fd87efa76f5d783dfb793bd4196f0
praisethemoon <doit@praisethemoon.org>
2023-09-17 19:21:52 +0200

    fix:tab size 2 -> 4

--- src/conf.ts
+++ src/conf.ts
@@ -1,7 +1,7 @@
 love.conf = (t) => {
-  t.window.title = "TypeScript Project";
-  t.window.width = 1200
-  t.window.height = 900
-  t.window.highdpi = true;
-  t.window.vsync = 0;
+    t.window.title = "TypeScript Project";
+    t.window.width = 1200
+    t.window.height = 900
+    t.window.highdpi = true;
+    t.window.vsync = 0;
 };
